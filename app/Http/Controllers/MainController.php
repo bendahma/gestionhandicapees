2fd042c9dbabf035d14d5c92a5699eb763dfb62e
Bendahma amine <mkaddourbendahma@gmail.com>
2020-08-11 19:17:50 +0100

    bug fixed

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,14 +30,10 @@ class MainController extends Controller
 
     public function dashboard()
     {
-        $hands = cache()->remember('HANDS_LISTS_ALL', 60*60*24 , function(){
-            return Hand::with(['paieinformation:hand_id,CCP','status:hand_id,status'])->withTrashed()->get(['id','nameFr','dob']);
-        });
+        $hands =  Hand::with(['paieinformation:hand_id,CCP','status:hand_id,status'])->withTrashed()->get(['id','nameFr','dob']);
 
         $user = Auth::user();
-
-        Debugbar::info(Auth::user());
-
+        
         return view('dashboard')
                     ->with('hands',$hands)
                     ->with('user',$user);
